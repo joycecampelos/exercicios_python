35a62bbc880760d24d386633692890a2ca17144d
joycecampelos <joycecampelos@gmail.com>
2022-05-22 18:13:58 -0300

    Update

--- faculdade/exercIcio01.py
+++ faculdade/exercIcio01.py
@@ -1,3 +1,7 @@
+'''
+EXERCÍCIO 01 - Faça um programa que peça o raio de um círculo, calcule e mostre sua área.
+'''
+
 raio = float(input('Digite o valor do raio: '))
 pi = 3.14
 area = pi * (raio ** 2)
